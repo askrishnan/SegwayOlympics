72de3a247dcb466d42de1cfb2841e2d26dde3cdd
Ana Krishnan <anupama.krishnan@students.olin.edu>
2017-11-11 17:25:22 -0500

    Adding our balancing code.

---
+++ Balance.cpp
@@ -0,0 +1,249 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
+#include <Wire.h>
+#include "Balance.h"
+
+int32_t gYZero;
+int32_t angle; // millidegrees
+int32_t angleRate; // degrees/s
+int32_t distanceLeft;
+int32_t speedLeft;
+int32_t driveLeft;
+int32_t distanceRight;
+int32_t displacement;
+
+int32_t speedRight;
+int32_t driveRight;
+int16_t motorSpeed;
+int32_t  angle_accum;
+int16_t angle_prev;
+bool isBalancingStatus;
+bool balanceUpdateDelayedStatus;
+extern int16_t testSpeed;          // this is the motor speed 
+
+#define KP  11
+#define KI 1
+#define KD 1
+
+
+bool isBalancing()
+{
+  return isBalancingStatus;
+}
+
+bool balanceUpdateDelayed()
+{
+  return balanceUpdateDelayedStatus;
+}
+
+void balanceSetup()
+{
+  // Initialize IMU.
+  Wire.begin();
+  if (!imu.init())
+  {
+    while(true)
+    {
+      Serial.println("Failed to detect and initialize IMU!");
+      delay(200);
+    }
+  }
+  imu.enableDefault();
+  imu.writeReg(LSM6::CTRL2_G, 0b01011000); // 208 Hz, 1000 deg/s
+
+  // Wait for IMU readings to stabilize.
+  delay(1000);
+
+  // Calibrate the gyro.
+  int32_t total = 0;
+  for (int i = 0; i < CALIBRATION_ITERATIONS; i++)
+  {
+    imu.read();
+    total += imu.g.y;
+    delay(1);
+  }
+
+  gYZero = total / CALIBRATION_ITERATIONS;
+}
+
+// This function contains the core algorithm for balancing a
+// Balboa 32U4 robot.
+void balance()
+{
+  // Adjust toward angle=0 with timescale ~10s, to compensate for
+  // gyro drift.  More advanced AHRS systems use the
+  // accelerometer as a reference for finding the zero angle, but
+  // this is a simpler technique: for a balancing robot, as long
+  // as it is balancing, we know that the angle must be zero on
+  // average, or we would fall over.
+  angle = angle * 999 / 1000;
+
+  // This variable measures how close we are to our basic
+  // balancing goal - being on a trajectory that would cause us
+  // to rise up to the vertical position with zero speed left at
+  // the top.  This is similar to the fallingAngleOffset used
+  // for LED feedback and a calibration procedure discussed at
+  // the end of Balancer.ino.
+  //
+  // It is in units of millidegrees, like the angle variable, and
+  // you can think of it as an angular estimate of how far off we
+  // are from being balanced.
+  int32_t risingAngleOffset = angleRate * ANGLE_RATE_RATIO + angle;
+
+  // Combine risingAngleOffset with the distance and speed
+  // variables, using the calibration constants defined in
+  // Balance.h, to get our motor response.  Rather than becoming
+  // the new motor speed setting, the response is an amount that
+  // is added to the motor speeds, since a *change* in speed is
+  // what causes the robot to tilt one way or the other.
+
+  angle_accum = angle_accum + angle;
+  
+  motorSpeed = (
+     -KP*angle - KI*angle_accum - KP*(angle-angle_prev)
+    ) / 100 / GEAR_RATIO;
+
+  angle_prev = angle; 
+  
+ // motorSpeed += (
+ //  + ANGLE_RESPONSE * risingAngleOffset
+ //   + DISTANCE_RESPONSE * (distanceLeft + distanceRight)
+ //   + SPEED_RESPONSE * (speedLeft + speedRight)
+ //   ) / 100 / GEAR_RATIO;
+
+  if (motorSpeed > MOTOR_SPEED_LIMIT)
+  {
+    motorSpeed = MOTOR_SPEED_LIMIT;
+  }
+  if (motorSpeed < -MOTOR_SPEED_LIMIT)
+  {
+    motorSpeed = -MOTOR_SPEED_LIMIT;
+  }
+
+  // Adjust for differences in the left and right distances; this
+  // will prevent the robot from rotating as it rocks back and
+  // forth due to differences in the motors, and it allows the
+  // robot to perform controlled turns.
+  int16_t distanceDiff = distanceLeft - distanceRight;
+
+  if(!DISABLE_MOTORS){
+   motors.setSpeeds(
+      motorSpeed + distanceDiff * DISTANCE_DIFF_RESPONSE / 100,
+     motorSpeed - distanceDiff * DISTANCE_DIFF_RESPONSE / 100);
+    }
+}
+
+void lyingDown()
+{
+  // Reset things so it doesn't go crazy.
+  motorSpeed = 0;
+  distanceLeft = 0;
+  distanceRight = 0;
+  motors.setSpeeds(0, 0);
+
+  if (angleRate > -6 && angleRate < 6)
+  {
+    // It's really calm, so we know the angles.
+    if (imu.a.z > 0)
+    {
+      
+      // this is based on coarse measurement of what I think the angle would be resting on the flat surface. 
+      // this corresponds to 94.8 degrees
+      angle = 94827-6000;
+  
+    }
+    else
+    {
+      // this is based on coarse measurement of what I think the angle would be resting on the flat surface. 
+         angle = -94827; 
+    }
+    distanceLeft = 0;
+    distanceRight = 0;
+  }
+}
+
+void integrateGyro()
+{
+  // Convert from full-scale 1000 deg/s to deg/s.
+  angleRate = (imu.g.y - gYZero) / 29;
+
+  angle += angleRate * UPDATE_TIME_MS;
+}
+
+void integrateEncoders()
+{
+  static int16_t lastCountsLeft;
+  int16_t countsLeft = encoders.getCountsLeft();
+  speedLeft = (countsLeft - lastCountsLeft);
+  distanceLeft += countsLeft - lastCountsLeft;
+  lastCountsLeft = countsLeft;
+
+  static int16_t lastCountsRight;
+  int16_t countsRight = encoders.getCountsRight();
+  speedRight = (countsRight - lastCountsRight);
+  distanceRight += countsRight - lastCountsRight;
+  lastCountsRight = countsRight;
+  if(testSpeed > 0)
+    displacement = displacement + distanceLeft;
+  else
+    displacement = displacement - distanceLeft;
+   
+    
+}
+
+void balanceDrive(int16_t leftSpeed, int16_t rightSpeed)
+{
+  driveLeft = leftSpeed;
+  driveRight = rightSpeed;
+}
+
+void balanceDoDriveTicks()
+{
+  distanceLeft -= driveLeft;
+  distanceRight -= driveRight;
+  speedLeft -= driveLeft;
+  speedRight -= driveRight;
+}
+
+void balanceResetEncoders()
+{
+  distanceLeft = 0;
+  distanceRight = 0;
+}
+
+void balanceUpdateSensors()
+{
+  imu.read();
+  integrateGyro();
+  integrateEncoders();
+}
+
+void balanceUpdate()
+{
+  static uint16_t lastMillis;
+  uint16_t ms = millis();
+
+  // Perform the balance updates at 100 Hz.
+  if ((uint16_t)(ms - lastMillis) < UPDATE_TIME_MS) { return; }
+  balanceUpdateDelayedStatus = ms - lastMillis > UPDATE_TIME_MS + 1;
+  lastMillis = ms;
+
+  balanceUpdateSensors();
+  balanceDoDriveTicks();
+
+  if (imu.a.x < 0)
+  {
+    lyingDown();
+    isBalancingStatus = false;
+  }
+  else
+  {
+    balance();
+    isBalancingStatus = true;
+  }
+}
---
+++ DontFall.ino
@@ -0,0 +1,229 @@
+// This code should help you get started with your balancing robot.
+// The code performs the following steps
+// Calibration phase:
+//  In this phase the robot should be stationary lying on the ground.
+//  The code will record the gyro data for a couple of seconds to zero
+//  out any gyro drift.
+//
+// Waiting phase:
+//  The robot will now start to integrate the gyro over time to estimate
+//  the angle.  Once the angle gets within +/- 3 degrees of vertical,
+//  we transition into the armed phase.  A buzzer will sound to indicate
+//  this transition.
+//
+// Armed phase:
+//  The robot is ready to go, however, it will not start executing its control
+//  loop until the angle leaves the region of [-3 degrees, 3 degrees].  This
+//  allows you to let go of your robot, and it won't start moving until it's started
+//  to fall just a little bit.  Once it leaves the region around vertical, it enters
+//  the controlled phase.
+//
+// Controlled phase:
+//  Here you can implement your control logic to do your balancing (or any of the
+//  other Olympic events.
+
+
+#include <Balboa32U4.h>
+#include <Wire.h>
+#include <LSM6.h>
+#include "Balance.h"
+
+#define METERS_PER_CLICK 3.141592*80.0*(1/1000.0)/12.0/(162.5)
+#define MOTOR_MAX 300
+#define MAX_SPEED 0.75  // m/s
+#define FORTY_FIVE_DEGREES_IN_RADIANS 0.78
+
+extern int32_t angle_accum;
+extern int32_t speedLeft;
+extern int32_t driveLeft;
+extern int32_t distanceRight;
+extern int32_t speedRight;
+extern int32_t distanceLeft;
+extern int32_t distanceRight;
+
+//Specifically for control
+const float Ji=5000;
+const float Jp=500;
+const float Ki=-25;
+const float Kp=-4;
+float vDesired=0;
+const float thetaDesired=0;
+
+void balanceDoDriveTicks();
+
+extern int32_t displacement;
+int32_t prev_displacement=0;
+
+LSM6 imu;
+Balboa32U4Motors motors;
+Balboa32U4Encoders encoders;
+Balboa32U4Buzzer buzzer;
+Balboa32U4ButtonA buttonA;
+
+uint32_t prev_time;
+
+void setup()
+{
+  Serial.begin(9600);
+  prev_time = 0;
+  ledYellow(0);
+  ledRed(1);
+  balanceSetup();
+  ledRed(0);
+  angle_accum = 0;
+  ledGreen(0);
+  ledYellow(0);
+}
+
+extern int16_t angle_prev;
+int16_t start_flag = 0;
+int16_t armed_flag = 0;
+int16_t start_counter = 0;
+void lyingDown();
+extern bool isBalancingStatus;
+extern bool balanceUpdateDelayedStatus;
+
+void newBalanceUpdate()
+{
+  static uint32_t lastMillis;
+  uint32_t ms = millis();
+
+  if ((uint32_t)(ms - lastMillis) < UPDATE_TIME_MS) { return; }
+  balanceUpdateDelayedStatus = ms - lastMillis > UPDATE_TIME_MS + 1;
+  lastMillis = ms;
+
+  // call functions to integrate encoders and gyros
+  balanceUpdateSensors();
+ 
+   if (imu.a.x < 0)
+  {
+    lyingDown();
+    isBalancingStatus = false;
+  }
+  else
+  {
+    isBalancingStatus = true;
+  }
+}
+
+
+float testSpeed = 0;          // this is the desired motor speed
+
+void loop()
+{
+  uint32_t cur_time = 0;
+  static uint32_t prev_print_time = 0;   // this variable is to control how often we print on the serial monitor
+  static float angle_rad;                // this is the angle in radians
+  static float angle_rad_accum = 0;      // this is the accumulated angle in radians
+  static float del_theta = 0; 
+  static float error_ = 0;      // this is the accumulated velocity error in m/s 
+  static float error_theta_accum=0;
+  static float error_left_accum = 0;      // this is the accumulated velocity error in m/s
+  static float error_right_accum = 0;      // this is the accumulated velocity error in m/s
+
+  cur_time = millis();                   // get the current time in miliseconds
+
+
+
+  newBalanceUpdate();                    // run the sensor updates. this function checks if it has been 10 ms since the previous 
+  
+  if(angle > 3000 || angle < -3000)      // If angle is not within +- 3 degrees, reset counter that waits for start
+  {
+    start_counter = 0;
+  }
+
+  bool shouldPrint = cur_time - prev_print_time > 105;
+  if(shouldPrint)   // do the printing every 105 ms. Don't want to do it for an integer multiple of 10ms to not hog the processor
+  {
+        Serial.print(angle_rad);   
+        Serial.print("\t");
+        Serial.print(angle);   
+        Serial.print("\t");
+        Serial.print(speedLeft);
+        Serial.print("\t");
+        Serial.print(angle_rad_accum);
+        Serial.println("testSpeed = ");
+        Serial.println(testSpeed);
+        Serial.println(imu.a.z);        
+        prev_print_time = cur_time;
+  }
+
+  float delta_t = (cur_time - prev_time)/1000.0;
+
+  // handle the case where this is the first time through the loop
+  if (prev_time == 0) {
+    delta_t = 0.01;
+  }
+  
+  // every UPDATE_TIME_MS, check if angle is within +- 3 degrees and we haven't set the start flag yet
+  if(cur_time - prev_time > UPDATE_TIME_MS && angle > -3000 && angle < 3000 && !armed_flag)   
+  {
+    // increment the start counter
+    start_counter++;
+    // If the start counter is greater than 30, this means that the angle has been within +- 3 degrees for 0.3 seconds, then set the start_flag
+    if(start_counter > 30)
+    {
+      armed_flag = 1;
+      buzzer.playFrequency(DIV_BY_10 | 445, 1000, 15);
+    }
+  }
+
+  // angle is in millidegrees, convert it to radians and subtract the desired theta
+  angle_rad = ((float)angle)/1000/180*3.14159 - del_theta;
+
+  // only start when the angle falls outside of the 3.0 degree band around 0.  This allows you to let go of the
+  // robot before it starts balancing
+  if(cur_time - prev_time > UPDATE_TIME_MS && (angle < -3000 || angle > 3000) && armed_flag)   
+  {
+    start_flag = 1;
+    armed_flag = 0;
+  }
+
+  // every UPDATE_TIME_MS, if the start_flag has been set, do the balancing
+  if(cur_time - prev_time > UPDATE_TIME_MS && start_flag)
+  {
+    // set the previous time to the current time for the next run through the loop
+    prev_time = cur_time;
+
+    // speedLeft and speedRight are just the change in the encoder readings
+    // wee need to do some math to get them into m/s
+    float vL = METERS_PER_CLICK*speedLeft/delta_t;
+    float vR = METERS_PER_CLICK*speedRight/delta_t;
+
+    //Insert Kai's best control effort here :) Good vibes only
+
+    //Theta control
+    float error_theta=thetaDesired-angle_rad;
+    error_theta_accum=error_theta_accum+(error_theta*delta_t);
+    vDesired=Kp*error_theta+Ki*error_theta_accum;
+    
+
+    //Velocity control
+    float error_L=vDesired-vL;
+    float error_R=vDesired-vR;
+    error_left_accum=error_left_accum+(error_L*delta_t);
+    error_right_accum=error_right_accum+(error_R*delta_t);
+ 
+    // set PWM_left and PWM_right here
+    float PWM_left=Jp*error_L+Ji*error_left_accum;
+    float PWM_right=Jp*error_R+Ji*error_right_accum;
+
+    // if the robot is more than 45 degrees, shut down the motor
+    if(start_flag && fabs(angle_rad) > FORTY_FIVE_DEGREES_IN_RADIANS) // TODO: this was set to angle < -0.78... I changd it to angle_rad
+    {
+      // reset the accumulated errors here
+      start_flag = 0;   /// wait for restart
+      prev_time = 0;
+      motors.setSpeeds(0, 0);
+    } else if(start_flag) {
+      motors.setSpeeds((int)PWM_left, (int)PWM_right);
+    }
+  }
+
+  // kill switch
+  if (buttonA.getSingleDebouncedPress())
+  {
+      motors.setSpeeds(0,0);
+      while(!buttonA.getSingleDebouncedPress());
+  }
+}
